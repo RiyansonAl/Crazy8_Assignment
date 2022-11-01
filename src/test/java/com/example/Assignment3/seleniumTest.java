8f136cc5d914f31da6cacb96163193e5224351bb
Riyanson Alfred <ralfr040@uottawa.ca>
2022-11-01 16:39:43 -0400

    U-Test: Tests for the main page of the game with the host game button and join game button

---
+++ src/test/java/com/example/Assignment3/seleniumTest.java
@@ -0,0 +1,73 @@
+package com.example.Assignment3;
+
+
+        import org.junit.jupiter.api.DisplayName;
+        import org.junit.jupiter.api.Test;
+        import org.openqa.selenium.By;
+        import org.openqa.selenium.WebDriver;
+        import org.openqa.selenium.WebElement;
+        import org.openqa.selenium.chrome.ChromeDriver;
+        import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class seleniumTest {
+    String webDriverLocation = "D:\\Users\\Riyanson\\Documents\\School\\Masters\\Fall 2022\\COMP 5104\\Assignments\\Assignment 3\\chromedriver_win32\\chromedriver.exe";
+    String webDriverType = "webdriver.chrome.driver";
+    @Test
+    @DisplayName("Selenium Test")
+    public void site_header_is_on_home_page() {
+        WebDriver browser;
+
+        System.setProperty("webdriver.chrome.driver", "D:\\Users\\Riyanson\\Documents\\School\\Masters\\Fall 2022\\COMP 5104\\Assignments\\Assignment 3\\chromedriver_win32\\chromedriver.exe");
+        browser = new ChromeDriver();
+        browser.get("http://saucelabs.com");
+        WebElement header = browser.findElement(By.id("headerMainNav"));
+        assertTrue((header.isDisplayed()));
+        browser.close();
+    }
+
+    @Test
+    @DisplayName("Spring boot Test")
+    public void spring_boot_test() {
+        WebDriver browser;
+
+        System.setProperty(webDriverType, webDriverLocation);
+        browser = new ChromeDriver();
+        browser.get("http://localhost:8080");
+        WebElement header = browser.findElement(By.id("nameForm"));
+        assertTrue((header.isDisplayed()));
+        browser.close();
+    }
+
+    @Test
+    @DisplayName("Game Host Main Page - Host")
+    public void gamehost_main_page() {
+        WebDriver browser;
+
+        System.setProperty(webDriverType, webDriverLocation);
+        browser = new ChromeDriver();
+        browser.get("http://localhost:8080");
+        WebElement header = browser.findElement(By.id("hostGameButton"));
+        assertTrue((header.isDisplayed()));
+        browser.close();
+    }
+
+    @Test
+    @DisplayName("Game Host Main Page- Join Game")
+    public void gamehost_main_page() {
+        WebDriver browser;
+
+        System.setProperty(webDriverType, webDriverLocation);
+        browser = new ChromeDriver();
+        browser.get("http://localhost:8080");
+        WebElement header = browser.findElement(By.id("joinGameButton"));
+        assertTrue((header.isDisplayed()));
+        browser.close();
+    }
+
+
+
+
+
+
+
+}
