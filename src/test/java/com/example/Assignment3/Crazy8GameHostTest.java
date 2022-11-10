1eca91e1c9cbc3381f2bb1eb3c6b92c3e1174d30
Riyanson Alfred <ralfr040@uottawa.ca>
2022-11-09 23:43:37 -0500

    U-TEST: JUnit test for playing a 8 and changing the suit

--- src/test/java/com/example/Assignment3/Crazy8GameHostTest.java
+++ src/test/java/com/example/Assignment3/Crazy8GameHostTest.java
@@ -54,7 +54,7 @@ class Crazy8GameHostTest {
     }
 
     @Test
-    @DisplayName("Player 1 plays a card and it becomes the next players turn")
+    @DisplayName("Player 1 plays a card")
     void playcardnormal() {
         Crazy8Player player1 = new Crazy8Player();
         Crazy8Player player2 = new Crazy8Player();
@@ -79,6 +79,36 @@ class Crazy8GameHostTest {
 
         host.setDiscardPile(discardPileTop);
         host.playCard(player1, riggedCard3);
+
+        assertEquals(player1HandString,host.printHand(player1));
+    }
+
+    @Test
+    @DisplayName("Player 1 plays an 8 and changes the suit to D")
+    void playcardnormal() {
+        Crazy8Player player1 = new Crazy8Player();
+        Crazy8Player player2 = new Crazy8Player();
+        Crazy8Player player3 = new Crazy8Player();
+        Crazy8Player player4 = new Crazy8Player();
+
+        Crazy8Player[] players = {player1, player2, player3, player4};
+
+        Crazy8GameHost host = new Crazy8GameHost(players);
+        //Five cards to draw
+        Card riggedCard1 = new Card(Card.Rank.EIGHT, Card.Suit.H);
+        Card riggedCard2 = new Card(Card.Rank.TEN, Card.Suit.C);
+        Card riggedCard3 = new Card(Card.Rank.EIGHT, Card.Suit.H);
+        Card riggedCard4 = new Card(Card.Rank.K, Card.Suit.D);
+        Card riggedCard5 = new Card(Card.Rank.Q, Card.Suit.S);
+
+        Card[] riggedHand = {riggedCard1, riggedCard2, riggedCard3, riggedCard4, riggedCard5};
+        Card[] player1Hand = host.drawHand(player1, riggedHand);
+        String player1HandString = "8H,10C,KD,QS";
+
+        Card discardPileTop = new Card(Card.Rank.SIX, Card.Suit.H);
+
+        host.setDiscardPile(discardPileTop);
+        host.playCard(player1, riggedCard3, Card.Suit.D);
 
         assertEquals(player1HandString,host.printHand(player1));
 
