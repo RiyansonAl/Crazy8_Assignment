bb65df45650f5f80aa8c1dd09f97a2c84739f136
Riyanson Alfred <ralfr040@uottawa.ca>
2022-11-10 00:07:08 -0500

    U-TEST: JUnit test for playing a card and the next player taking a turn

--- src/test/java/com/example/Assignment3/Crazy8GameHostTest.java
+++ src/test/java/com/example/Assignment3/Crazy8GameHostTest.java
@@ -115,6 +115,51 @@ class Crazy8GameHostTest {
 
     }
 
+    @Test
+    @DisplayName("Player 1 plays a card and then the next player plays a card")
+    void playcardnormal() {
+        Crazy8Player player1 = new Crazy8Player();
+        Crazy8Player player2 = new Crazy8Player();
+        Crazy8Player player3 = new Crazy8Player();
+        Crazy8Player player4 = new Crazy8Player();
+
+        Crazy8Player[] players = {player1, player2, player3, player4};
+
+        Crazy8GameHost host = new Crazy8GameHost(players);
+        //Five cards to draw - player 1
+        Card riggedCard1 = new Card(Card.Rank.EIGHT, Card.Suit.H);
+        Card riggedCard2 = new Card(Card.Rank.TEN, Card.Suit.C);
+        Card riggedCard3 = new Card(Card.Rank.SEVEN, Card.Suit.H);
+        Card riggedCard4 = new Card(Card.Rank.K, Card.Suit.D);
+        Card riggedCard5 = new Card(Card.Rank.Q, Card.Suit.S);
+
+        Card[] riggedHand = {riggedCard1, riggedCard2, riggedCard3, riggedCard4, riggedCard5};
+        Card[] player1Hand = host.drawHand(player1, riggedHand);
+        String player1HandString = "8H,10C,KD,QS";
+
+        //Five cards to draw - player 2
+        Card riggedCard6 = new Card(Card.Rank.NINE, Card.Suit.D);
+        Card riggedCard7 = new Card(Card.Rank.SEVEN, Card.Suit.D);
+        Card riggedCard8 = new Card(Card.Rank.TWO, Card.Suit.C);
+        Card riggedCard9 = new Card(Card.Rank.J, Card.Suit.D);
+        Card riggedCard10 = new Card(Card.Rank.K, Card.Suit.S);
+
+        Card[] riggedHan2 = {riggedCard6, riggedCard7, riggedCard8, riggedCard9, riggedCard10};
+        Card[] player2Hand = host.drawHand(player1, riggedHan2);
+        String player2HandString = "9D,2C,JD,KS";
+
+        Card discardPileTop = new Card(Card.Rank.SIX, Card.Suit.H);
+
+        host.setDiscardPile(discardPileTop);
+        host.playCard(player1, riggedCard3, Card.Suit.NONE);
+        Crazy8Player nextPlayer = host.getNextPlayer();
+        host.playCard(nextPlayer, riggedCard7, Card.Suit.NONE);
+
+        assertEquals(player1HandString,host.printHand(player1));
+        assertEquals(player2HandString,host.printHand(player2));
+
+    }
+
 
 
 
