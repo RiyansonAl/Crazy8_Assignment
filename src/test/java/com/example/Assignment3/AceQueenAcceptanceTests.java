8e000d304df30eeddb70a4a81cefe67385c4bac0
Riyanson Alfred <ralfr040@uottawa.ca>
2022-12-04 17:13:16 -0500

    A-TEST: Wrote the Acceptance Test on line 48

--- src/test/java/com/example/Assignment3/AceQueenAcceptanceTests.java
+++ src/test/java/com/example/Assignment3/AceQueenAcceptanceTests.java
@@ -558,6 +558,133 @@ public class AceQueenAcceptanceTests {
 
     }
 
+    @Test
+    @DisplayName("Plays game up to player 4 and then player 4 plays a Queen and skips player 1's turn")
+    public void acceptanceTestLine48() {
+        WebDriver hostBrowser;
+        WebDriver player2Browser;
+        WebDriver player3Browser;
+        WebDriver player4Browser;
+        WebDriver riggingBrowser;
+
+        System.setProperty(webDriverType, webDriverLocation);
+        hostBrowser = new ChromeDriver();
+        hostBrowser.get("http://localhost:8080");
+
+        //Player 1/Host starting and Joining the game
+        hostBrowser.findElement(By.id("hostButton")).sendKeys(Keys.ENTER);
+        hostBrowser.findElement(By.id("startGameButton")).sendKeys(Keys.ENTER);
+
+        //Player 2 joining the Game
+        player2Browser = new ChromeDriver();
+        player2Browser.get("http://localhost:8080");
+        player2Browser.findElement(By.id("joinGameButton")).sendKeys(Keys.ENTER);
+        player2Browser.findElement(By.id("continueButton")).sendKeys(Keys.ENTER);
+
+        //Player 3 Joining the Game
+        player3Browser = new ChromeDriver();
+        player3Browser.get("http://localhost:8080");
+        player3Browser.findElement(By.id("joinGameButton")).sendKeys(Keys.ENTER);
+        player3Browser.findElement(By.id("continueButton")).sendKeys(Keys.ENTER);
+
+        //Player 4 Joining the Game
+        player4Browser = new ChromeDriver();
+        player4Browser.get("http://localhost:8080");
+        player4Browser.findElement(By.id("joinGameButton")).sendKeys(Keys.ENTER);
+        player4Browser.findElement(By.id("continueButton")).sendKeys(Keys.ENTER);
+
+        //Sleep for 3 seconds
+        try {
+            Thread.sleep(25000);
+        } catch (InterruptedException ex) {
+            throw new RuntimeException(ex);
+        }
+
+        //Rigg the all player's cards
+        riggingBrowser = new ChromeDriver();
+        riggingBrowser.get("http://localhost:8080/riggingPage");
+        String DiscardPile = "4C";
+        String Player1HandRigged = "3D,6D,7C,5H";
+        riggingBrowser.findElement(By.id("discardPileText")).sendKeys(DiscardPile);
+        riggingBrowser.findElement(By.id("discardPileButton")).sendKeys(Keys.ENTER);
+        riggingBrowser.findElement(By.id("Player1HandText")).sendKeys(Player1HandRigged);
+        riggingBrowser.findElement(By.id("Player1HandButton")).sendKeys(Keys.ENTER);
+        String Player2HandRigged = "5C,6H,7H,5D";
+        riggingBrowser.findElement(By.id("Player2HandText")).sendKeys(Player2HandRigged);
+        riggingBrowser.findElement(By.id("Player2HandButton")).sendKeys(Keys.ENTER);
+        String Player3HandRigged = "6C,4H,8H,3D";
+        riggingBrowser.findElement(By.id("Player3HandText")).sendKeys(Player3HandRigged);
+        riggingBrowser.findElement(By.id("Player3HandButton")).sendKeys(Keys.ENTER);
+        String Player4HandRigged = "QC,9H,9H,9D";
+        riggingBrowser.findElement(By.id("Player4HandText")).sendKeys(Player4HandRigged);
+        riggingBrowser.findElement(By.id("Player4HandButton")).sendKeys(Keys.ENTER);
+        //Sleep for 5 seconds
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException ex) {
+            throw new RuntimeException(ex);
+        }
+
+        //Player 1 play card
+        String Player1CardPlayed = "7C";
+        hostBrowser.findElement(By.id("playerCardField")).sendKeys(Player1CardPlayed);
+        hostBrowser.findElement(By.id("playerCardButton")).sendKeys(Keys.ENTER);
+        //Sleep for 5 seconds
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException ex) {
+            throw new RuntimeException(ex);
+        }
+
+        //Player 2 play card
+        String Player2CardPlayed = "5C";
+        player2Browser.findElement(By.id("playerCardField")).sendKeys(Player2CardPlayed);
+        player2Browser.findElement(By.id("playerCardButton")).sendKeys(Keys.ENTER);
+        //Sleep for 5 seconds
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException ex) {
+            throw new RuntimeException(ex);
+        }
+
+        //Player 3 play card
+        String Player3CardPlayed = "6C";
+        player3Browser.findElement(By.id("playerCardField")).sendKeys(Player3CardPlayed);
+        player3Browser.findElement(By.id("playerCardButton")).sendKeys(Keys.ENTER);
+        //Sleep for 5 seconds
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException ex) {
+            throw new RuntimeException(ex);
+        }
+
+        //Player 4 play card
+        String Player4CardPlayed = "QC";
+        player4Browser.findElement(By.id("playerCardField")).sendKeys(Player4CardPlayed);
+        player4Browser.findElement(By.id("playerCardButton")).sendKeys(Keys.ENTER);
+
+
+        //Sleep for 10 seconds to allow all the pages to refresh
+        try {
+            Thread.sleep(10000);
+        } catch (InterruptedException ex) {
+            throw new RuntimeException(ex);
+        }
+        //Check the card was played
+        WebElement player1Text = hostBrowser.findElement(By.xpath("//*[text() != '']"));
+        String player1Message = "Player 2 Turn";
+        System.out.println(player1Text.getText());
+        assertTrue((player1Text.getText().contains(player1Message)));
+
+
+        riggingBrowser.quit();
+        player2Browser.quit();
+        player3Browser.quit();
+        player4Browser.quit();
+        hostBrowser.quit();
+
+    }
+
 
 
 }
